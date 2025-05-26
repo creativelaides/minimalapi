a5be1ebab41b02dc291f406fd35e3e85b6dc0529
Jose Velaides <creativelaides@gmail.com>
2025-05-26 13:28:28 -0500

    feat: :fire: cambios de propiedades en Signature

--- Entities/Signature.cs
+++ Entities/Signature.cs
@@ -3,8 +3,8 @@ namespace minimalapi.Entities;
 public class Signature
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string ImageUrl { get; set; } = string.Empty;
-    public string Category { get; set; } = string.Empty;
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Category { get; set; }
 }
