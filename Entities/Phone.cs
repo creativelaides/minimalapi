b898431dc2a2bd7a4d990aa12dfa337b3fefcadd
Jose Velaides <creativelaides@gmail.com>
2025-06-04 13:23:49 -0500

    feat: :fire: se añade un object value phone

---
+++ Entities/Phone.cs
@@ -0,0 +1,18 @@
+namespace minimalapi.Entities;
+
+public record Phone
+{
+    public int Id { get; set; }
+    public required string Number { get; set; }
+    public required string Type { get; set; } // e.g., "Mobile", "Home", "Work"
+
+    public override string ToString()
+    {
+        return $"{Id}: {Number} ({Type})";
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Number, Type);
+    }
+}
