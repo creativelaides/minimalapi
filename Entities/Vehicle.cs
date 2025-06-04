66a3e132bd13815ea0d061a6dfa1f6c424e85a68
Jose Velaides <creativelaides@gmail.com>
2025-06-04 13:34:51 -0500

    feat: :zap: nueva entidad signature

---
+++ Entities/Vehicle.cs
@@ -0,0 +1,11 @@
+namespace minimalapi.Entities;
+
+public class Vehicle(string vim, string make, Person owner, string model, int year)
+{
+    public string Vim { get; set; } = vim;
+    public string Make { get; set; } = make;
+    public Person Owner { get; set; } = owner;
+
+    public string Model { get; set; } = model;
+    public int Year { get; set; } = year;
+}
