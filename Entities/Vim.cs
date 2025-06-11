e973f44876a2bbcbbfe0bcef9338ab9955d5f43f
Jose Velaides <creativelaides@gmail.com>
2025-06-11 13:23:37 -0500

    feat: Create Vim.cs

---
+++ Entities/Vim.cs
@@ -0,0 +1,9 @@
+namespace minimalapi.Entities;
+
+public class Vim
+{
+    public int Id { get; set; }
+    public string? Number { get; set; }
+    public string? Description { get; set; }
+    public string? Owner { get; set; }
+}
